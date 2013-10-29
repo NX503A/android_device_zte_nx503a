0af3ffcb64fc2c9086754c9331d6e977eb3623c5
Damir Didjusto <damird@codeaurora.org>
2013-10-28 17:08:54 -0700

    msm8974: Add AUXPCM mixer paths file
    Add mixer paths file for targets that use Atheros BT.
    The mixer paths file has AUXPCM routing instead of
    Internal BT SCO routing that is required for RIVA FM chip.
    
    Change-Id: Ibfaae5573abd6a5786579a52212121798b0aaf5c

--- device.mk
+++ device.mk
@@ -82,7 +82,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_auxpcm.xml:system/etc/mixer_paths_auxpcm.xml
 
 # Bash from stock firmware
 PRODUCT_COPY_FILES += \
