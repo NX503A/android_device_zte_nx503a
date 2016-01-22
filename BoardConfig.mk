076ef8f0dfddbe027052be0faa41e1e5710df59f
BambooIV <ningdisheng@gmail.com>
2016-01-23 01:52:58 +0800

    nx503a: Update graphics flags

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_VARIANT := krait
 
 TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 TARGET_BOOTLOADER_BOARD_NAME := nx503a
 TARGET_BOARD_INFO_FILE := device/zte/nx503a/board-info.txt
 
@@ -59,15 +60,22 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 2684354560
 BOARD_FLASH_BLOCK_SIZE := 131072
 
 # Graphics
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
 HAVE_ADRENO_SOURCE := false
-MAX_EGL_CACHE_KEY_SIZE := 12*1024
-MAX_EGL_CACHE_SIZE := 2048*1024
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+TARGET_FORCE_HWC_FOR_VIRTUAL_DISPLAYS := true
 TARGET_USES_ION := true
 USE_OPENGL_RENDERER := true
-SF_VSYNC_EVENT_PHASE_OFFSET_NS := 5000000
-VSYNC_EVENT_PHASE_OFFSET_NS := 7500000
+
+# Shader cache config options
+# Maximum size of the  GLES Shaders that can be cached for reuse.
+# Increase the size if shaders of size greater than 12KB are used.
+MAX_EGL_CACHE_KEY_SIZE := 12*1024
+
+# Maximum GLES shader cache size for each app to store the compiled shader
+# binaries. Decrease the size if RAM or Flash Storage size is a limitation
+# of the device.
+MAX_EGL_CACHE_SIZE := 2048*1024
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
